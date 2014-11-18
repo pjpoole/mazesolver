529a72b481260ee5363a5aecb8ef724f25b2cac0
Peter Poole <peter.j.poole@gmail.com>
2014-11-18 09:06:29 -0500

    Create initial class structure.

---
+++ mazegenerator.rb
@@ -0,0 +1,19 @@
+class MazeGenerator
+
+  HEIGHT, WIDTH = 20, 20
+
+  # initialize:
+  # fill with stars
+  def initialize(height = HEIGHT, width = WIDTH)
+    @board = generate(height, width)
+
+  end
+
+  # Pick start, end
+
+  # Flood fill branches
+  # TODO (branching factor)
+
+  # Output to file
+
+end
